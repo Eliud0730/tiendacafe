d379797c386c28410f878d73a1be92535f916f8d
Eliud0730 <eliud0730@gmail.com>
2020-03-03 13:32:12 -0700

    first commit

---
+++ app/etc/env.php
@@ -0,0 +1,71 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin_ixy1ch'
+    ],
+    'crypt' => [
+        'key' => 'a93f18f3357b34e3cae519befdd9faed'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'cafe-cometa',
+                'username' => 'root',
+                'password' => '',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '2f8_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '2f8_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'google_product' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Wed, 29 Jan 2020 03:19:14 +0000'
+    ]
+];
